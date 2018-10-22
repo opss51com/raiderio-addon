b2832aed86a2a68f0eb39a38d5e1bc3180ebadf9
Jah Raphael <jahraphael@yahoo.com>
2018-10-22 00:11:56 -0500

    initial integration of raid support!

---
+++ db/db_raiding_cn_alliance_characters.lua
@@ -0,0 +1,6 @@
+local provider={name=...,data=2,region="cn",faction=1,date="2018-10-22T04:25:37Z",currentRaid={["name"]="Uldir",["shortName"]="ULD",["bossCount"]=8},previousRaid=nil,db1={}}
+local F
+
+
+F = nil
+RaiderIO.AddProvider(provider)
